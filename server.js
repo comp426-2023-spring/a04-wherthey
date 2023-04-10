72dbba4d9f3acfdbc4294a1b5813743d34303e52
Nick Wherthey <nwherthey@unc.edu>
2023-04-09 22:39:12 -0400

    foo

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import minimist from 'minimist'
 import express from 'express'
 
 const args = minimist(process.argv.slice(2))
-const PORT = args.port || 5000
+const port = args.port || 5000
 const app = express()
 
 app.use(express.json)
