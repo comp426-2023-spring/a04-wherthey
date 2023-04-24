5b45fbd7c1597389738eb410e64d37de5a8e3929
Nick Wherthey <nwherthey@unc.edu>
2023-04-24 14:45:22 -0400

    foo

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
 
-import { rps, rpsls} from "./lib/rpsls.js"
 import minimist from 'minimist'
-import express from 'express'
+import express, { json, urlencoded } from 'express'
+import { rps, rpsls } from './lib/rpsls.js'
 
 const args = minimist(process.argv.slice(2))
 const port = args.port || 5000
 const app = express()
 
-app.use(express.json)
-app.use(express.urlencoded({extended: true}))
+app.use(json)
+app.use(urlencoded({extended: true}))
 
 app.get('/app', (req, res) => {
 	res.status(200).send("200 OK")
