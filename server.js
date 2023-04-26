e8dc225c787b8e9f68e320cb4a8639f061aaa56e
Nick Wherthey <nwherthey@unc.edu>
2023-04-26 16:32:35 -0400

    foo

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ import minimist from 'minimist'
 import express, { json, urlencoded } from 'express'
 import { rps, rpsls } from './lib/rpsls.js'
 
-const minimist = minimist()
 const args = minimist(process.argv.slice(2))
 const port = args.port || 5000
 const app = express()
